bad9c4a807aaab4dbeaae666bc473657e1b99a1d
Ben Ubois <ben@benubois.com>
2019-03-12 23:17:41 -0700

    Tests

--- app/server.js
+++ app/server.js
@@ -1,9 +1,9 @@
-const App = require("./app");
-const Server = App.listen((process.env.PORT || 3000));
+const app = require("./app");
+const server = app.listen((process.env.PORT || 3000));
 
 process.on("SIGINT", () => {
     if (process.env.NODE_ENV === "production") {
-        Server.close(function(error) {
+        server.close(function(error) {
             console.error("SIGINT received, shutting down");
             if (error) {
                 console.error(err);
