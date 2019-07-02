dd3f2ba9573bbe6abf5611a9f48f653e876dbab5
Ben Ubois <ben@benubois.com>
2019-07-02 12:33:39 +0200

    Dependency updates.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,2 @@
 server 'extract1.feedbin.com', user: 'app', roles: %w{app}
 server 'extract2.feedbin.com', user: 'app', roles: %w{app}
-server 'extract3.feedbin.com', user: 'app', roles: %w{app}
-server 'extract4.feedbin.com', user: 'app', roles: %w{app}
-server 'extract5.feedbin.com', user: 'app', roles: %w{app}
