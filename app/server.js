0cb4575d7e4c81e2d1c705110db48b23c89e18f9
Karl Prieb <karl@prieb.me>
2022-08-01 16:31:29 -0300

    Add logs

--- app/server.js
+++ app/server.js
@@ -1,16 +1,19 @@
 const app = require("./app");
-const server = app.listen((process.env.PORT || 3000));
+const serverPort = process.env.PORT || 3000;
+const server = app.listen(serverPort, () => {
+    console.log(`Extract started on port ${serverPort}`);
+});
 
 process.on("SIGINT", () => {
     if (process.env.NODE_ENV === "production") {
-        server.close(function(error) {
+        server.close(function (error) {
             console.error("SIGINT received, shutting down");
             if (error) {
                 console.error(err);
                 process.exit(1);
             }
-        })
+        });
     } else {
         process.exit(0);
     }
-})
+});
