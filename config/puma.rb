b4d10f90264336aa832cfdce35dbd2ecce919a9f
Ben Ubois <ben@benubois.com>
2023-01-26 15:50:27 +0100

    Update config

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,10 @@
 require "etc"
 
 app_directory = File.expand_path("..", __dir__)
-shared_directory = File.join(app_directory, "shared")
 
-bind "unix://#{shared_directory}/tmp/puma.sock"
-pidfile "#{shared_directory}/tmp/puma.pid"
-state_path "#{shared_directory}/tmp/puma.state"
+bind "unix://#{app_directory}/tmp/puma.sock"
+pidfile "#{app_directory}/tmp/puma.pid"
+state_path "#{app_directory}/tmp/puma.state"
 
 environment ENV.fetch("RACK_ENV", "development")
 workers ENV.fetch("WEB_CONCURRENCY", Etc.nprocessors).to_i
